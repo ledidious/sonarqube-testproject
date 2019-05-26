a70cc995efd2e2e7154ba785f96ca44832e170b0
ledidious <ledidious@gmail.com>
2019-05-26 18:59:29 +0200

    Added test c# script

---
+++ testFile.cs
@@ -0,0 +1,13 @@
+
+namespace Hello
+{
+    public class FirstClass {
+        public static void Main() {
+            var hello = "aa";
+
+            if (true) {
+                hello = "aa";
+            }
+        }
+    }
+}
